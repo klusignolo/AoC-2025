f6213e392cd340c99374dc9657d278a668dbb291
Pegasus\kevin <kevinlusignolo@gmail.com>
2025-12-02 23:35:44 -0600

    rename files

--- day 03 - lobby/part_one.py
+++ day 03 - lobby/part_one.py
@@ -1,16 +1,12 @@
-with open("input.txt") as f:
+with open("test_input.txt") as f:
     battery_banks = f.read().splitlines()
 
 joltage_total = 0
-BATTERY_COUNT = 12
 for battery_bank in battery_banks:
     batteries = [int(battery) for battery in list(battery_bank)]
-
-    best_batteries = []
-    left_index = 0
-    for i in range(BATTERY_COUNT,0,-1):
-        highest_joltage = max(batteries[left_index:len(batteries)-i+1])
-        left_index = batteries.index(highest_joltage, left_index, len(batteries))+1
-        best_batteries.append(highest_joltage)
-    joltage_total += int("".join(map(str, best_batteries)))
-print(joltage_total)
+    highest_joltage = max(batteries[:len(batteries)-1])
+    highest_index = batteries.index(highest_joltage)
+    second_highest_joltage = max(batteries[highest_index+1:])
+    max_joltage = int(f"{highest_joltage}{second_highest_joltage}")
+    joltage_total += max_joltage
+print(joltage_total)    
